7814e714edcc465b42b0f84a2651f621f460c2e4
yinm (numacchi, Yusuke Iinuma) <yinm@pepabo.com>
2017-06-23 08:08:06 +0900

    無限ループを入れることで、イベントループを止める

---
+++ chapter07/event_loop4.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World');
+});
+
+server.listen(1337, () => {
+  console.log('Server listening');
+});
+
+while(true);
