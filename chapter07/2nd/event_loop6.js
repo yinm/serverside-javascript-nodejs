cbe3a05d2e407b0c46b4ba26d278b425e5cdcd18
yinm (numacchi, Yusuke Iinuma) <yinm@pepabo.com>
2017-06-23 09:23:06 +0900

    process.nextTick() を使うことで、リスナの設定でemitできるようにする

---
+++ chapter07/2nd/event_loop6.js
@@ -0,0 +1,15 @@
+const events = require('events');
+const util = require('util');
+
+function AsyncEmitter() {
+  process.nextTick(() => {
+    this.emit('bar');
+  });
+}
+
+util.inherits(AsyncEmitter, events.EventEmitter);
+const foo = new AsyncEmitter();
+
+foo.on('bar', () => {
+  console.log('bar event emitted');
+});
