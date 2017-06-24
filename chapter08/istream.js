822d5f31a04e8b809dc1b7618d67892fae9ddcd9
yinm <yinm.cminor9@gmail.com>
2017-06-24 10:22:27 +0900

    readStream

---
+++ chapter08/istream.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const fs = require('fs');
+
+const filePath = path.join(__dirname, 'test.txt');
+
+const readStream = fs.createReadStream(filePath);
+readStream.on('data', (data) => {
+  console.log(data);
+});
+readStream.on('end', () => {
+  console.log('end');
+});
+readStream.on('error', (err) => {
+  console.log('An error occurred');
+  console.log(err);
+});
