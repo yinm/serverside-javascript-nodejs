d0c875b1832fd5d789643c73a03251324b231b41
yinm <yinm.cminor9@gmail.com>
2017-06-24 10:23:08 +0900

    encode設定

--- chapter08/istream.js
+++ chapter08/istream.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const filePath = path.join(__dirname, 'test.txt');
 
 const readStream = fs.createReadStream(filePath);
+readStream.setEncoding('utf8');
 readStream.on('data', (data) => {
   console.log(data);
 });
